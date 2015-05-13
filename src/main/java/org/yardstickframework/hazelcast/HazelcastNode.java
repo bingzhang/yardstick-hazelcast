e6f41a41c6c427511b130c31c60f8d0cd74c6a1c
nikolay_tikhonov <ntikhonov@gridgain.com>
2015-05-13 12:12:57 +0300

    Print configuration.

--- src/main/java/org/yardstickframework/hazelcast/HazelcastNode.java
+++ src/main/java/org/yardstickframework/hazelcast/HazelcastNode.java
@@ -63,6 +63,7 @@ public class HazelcastNode implements BenchmarkServer {
             hz = HazelcastClient.newHazelcastClient(clientCfg);
 
             println(cfg, "Hazelcast client started.");
+            println(cfg, "Hazelcast client configuration: [" + cfg + "].");
         }
         else {
             for (Map.Entry<String, String> env : System.getenv().entrySet())
@@ -76,6 +77,7 @@ public class HazelcastNode implements BenchmarkServer {
             hz = Hazelcast.newHazelcastInstance(hzCfg);
 
             println(cfg, "Hazelcast member started.");
+            println(cfg, "Hazelcast benchmark configuration: [" + cfg + "].");
         }
 
         assert hz != null;
